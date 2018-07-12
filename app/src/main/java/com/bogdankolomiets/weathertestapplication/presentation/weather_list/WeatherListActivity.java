b2b3aada7455317a89e69c8b521e059416e908cd
bkolomiets <not_my_email@gmail.com>
2018-07-12 20:50:52 +0300

    change resources locations

--- app/src/main/java/com/bogdankolomiets/weathertestapplication/presentation/weather_list/WeatherListActivity.java
+++ app/src/main/java/com/bogdankolomiets/weathertestapplication/presentation/weather_list/WeatherListActivity.java
@@ -3,4 +3,5 @@ package com.bogdankolomiets.weathertestapplication.presentation.weather_list;
 import com.bogdankolomiets.weathertestapplication.presentation.common.BaseActivity;
 
 public class WeatherListActivity extends BaseActivity {
+
 }
